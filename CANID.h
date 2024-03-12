ac48910b30135b33f8e5cdcc7317108d0b1c6f78
nihinihikun <107825036+nihinihikun@users.noreply.github.com>
2024-03-12 11:58:29 +0900

    ID整理

--- CANID.h
+++ CANID.h
@@ -2,8 +2,15 @@
 #define CANID_H
 
 // char
-#define CCP_EMST_mesure 0x001
-#define CCP_nose_status 0x010
+#define CCP_nose_status 0x002
+#define CCP_nose_surface_pressure1_status 0x003
+#define CCP_nose_surface_pressure2_status 0x004
+#define CCP_nose_surface_pressure3_status 0x005
+#define CCP_nose_surface_pressure4_status 0x006
+#define CCP_nose_surface_pressure5_status 0x007
+#define CCP_nose_surface_pressure6_status 0x008
+
+#define CCP_nose_adc 0x00A
 
 // uint32_t
 #define CCP_nose_temperature 0x040
@@ -11,8 +18,16 @@
 #define CCP_nose_adc 0x042
 
 // float
+#define CCP_nose_temperature 0x080
+#define CCP_nose_barometic_pressure 0x081
+#define CCP_nose_voltage 0x082
 
-// fp16
+#define CCP_surface_pressure1_pressure_pa 0x08A
+#define CCP_surface_pressure2_pressure_pa 0x08B
+#define CCP_surface_pressure3_pressure_pa 0x08C
+#define CCP_surface_pressure4_pressure_pa 0x08D
+#define CCP_surface_pressure5_pressure_pa 0x08E
+#define CCP_surface_pressure6_pressure_pa 0x08F
 
 #endif
 
